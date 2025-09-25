b84901830d16a92cd1617236fcf6be56323e9bcd
Leonard Ampoe <leonard.ampoe@student.rocva.nl>
2025-09-25 12:17:26 +0200

    Beginning of form

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,51 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
+
+function MyForm() {
+
+  const [selectedSex, setSelectedSex] = useState('banana');
+
+  const handleChange = (event) => {
+    setSelectedSex(event.target.value);
+  };
+
+  return (
+    <form>
+      <p>Aanhef:</p>
+      <label htmlFor=""> Dhr.
+        <input type="radio" name="Dhr." id="" value="M" checked={selectedSex === 'Dhr.'} onChange={handleChange}/>
+      </label>
+      <label htmlFor="">Mevr.
+        <input type="radio" name="Mevr." id="" value="V" checked={selectedSex === 'Mevr.'} onChange={handleChange}/>
+      </label>
+      <label htmlFor="">Onbekend
+        <input type="radio" name="Onbekend" id="" value="X" checked={selectedSex === ' '} onChange={handleChange}/>
+      </label>
+      <label>Naam:
+        <input type="text" required placeholder='Voornaam'/>
+        <input type="text" required placeholder='Tussenvoegsel(S)'/>
+        <input type="text" required placeholder='Achternaam'/>
+      </label>
+      <button type="submit">Verder</button>
+    </form>
+  )
+}
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Aanmelding</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </>, 
+    MyForm
   )
 }
 
